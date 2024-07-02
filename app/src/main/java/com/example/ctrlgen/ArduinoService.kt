71f19327b7104094ea4b2b91a64b77ee18c323d5
Natnael <107844070+Natcod@users.noreply.github.com>
2024-07-02 16:50:29 +0300

    Remove unused files and refactor code structure

--- app/src/main/java/com/example/ctrlgen/ArduinoService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ctrlgen
-
-// ArduinoService.kt
-import retrofit2.http.GET
-
-interface ArduinoService {
-    @GET("/sensor/data") // Adjust endpoint path as per your API
-    suspend fun getSensorData(): SensorData // Adjust return type as per your API response
-}
