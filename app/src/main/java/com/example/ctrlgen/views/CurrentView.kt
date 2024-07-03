be4e680895d31e6fe24dada663cd3866efd08cf6
Natnael <107844070+Natcod@users.noreply.github.com>
2024-07-03 14:26:07 +0300

    Add androidx.compose.ui.tooling.preview.Preview annotation to CurrentView.kt

--- app/src/main/java/com/example/ctrlgen/views/CurrentView.kt
+++ app/src/main/java/com/example/ctrlgen/views/CurrentView.kt
@@ -2,6 +2,7 @@ package com.example.ctrlgen.views
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.Description
@@ -9,6 +10,7 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 
+@Preview
 @Composable
 fun CurrentView(label: String, value: Float, modifier: Modifier = Modifier) {
     LineChartView(label, value, modifier)
