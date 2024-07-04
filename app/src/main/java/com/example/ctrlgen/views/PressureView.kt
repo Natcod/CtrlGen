6ddd41d203b00b4c974599517f42f2267a14464c
Natnael <107844070+Natcod@users.noreply.github.com>
2024-07-04 18:03:25 +0300

    chore: PressureView to accept a list of values for line chart

--- app/src/main/java/com/example/ctrlgen/views/PressureView.kt
+++ app/src/main/java/com/example/ctrlgen/views/PressureView.kt
@@ -2,15 +2,8 @@ package com.example.ctrlgen.views
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 
 @Composable
 fun PressureView(label: String, value: Float, modifier: Modifier = Modifier) {
-    LineChartView(label, value, modifier)
+    LineChartView(label, listOf(value), modifier)
 }
-
