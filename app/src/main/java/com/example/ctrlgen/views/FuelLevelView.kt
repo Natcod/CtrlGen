77fde4555d61d90f140ec9036c270fb3958f5854
Natnael <107844070+Natcod@users.noreply.github.com>
2024-07-04 14:00:21 +0300

    Refactor and  Add FuelLevelView composable for displaying fuel level chart

--- app/src/main/java/com/example/ctrlgen/views/FuelLevelView.kt
+++ app/src/main/java/com/example/ctrlgen/views/FuelLevelView.kt
@@ -1,16 +1,38 @@
 package com.example.ctrlgen.views
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.ctrlgen.R
+import androidx.compose.ui.tooling.preview.Preview
 
+
+@Preview
 @Composable
-fun FuelLevelView(label: String, value: Float, modifier: Modifier = Modifier) {
-    LineChartView(label, value, modifier)
+fun FuelLevelView(
+    label: String = "Fuel Level",
+    value: Float = 50f,
+    modifier: Modifier = Modifier,
+    chartSize: Dp = 200.dp
+) {
+    val sliceSpace = 3f
+    val holeRadius = 90f
+    val transparentCircleRadius = 55f
+    val holeColor = colorResource(id = R.color.varC).toArgb()
+    val centerTextSize = 24f
+
+    LevelView(
+        label = label,
+        value = value,
+        chartSize = chartSize,
+        sliceSpace = sliceSpace,
+        holeRadius = holeRadius,
+        transparentCircleRadius = transparentCircleRadius,
+        holeColor = holeColor,
+        centerTextSize = centerTextSize,
+        modifier = modifier
+    )
 }
 
